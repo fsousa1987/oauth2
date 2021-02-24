820e5fb0528f93e5fda1e708babdbf9b1d5b4869
Francisco Sousa <franciscoeds1987@gmail.com>
2021-02-24 15:31:26 -0300

    Project created

---
+++ src/test/java/com/appsdeveloperblog/ws/api/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.appsdeveloperblog.ws.api.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SuppressWarnings("EmptyMethod")
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
