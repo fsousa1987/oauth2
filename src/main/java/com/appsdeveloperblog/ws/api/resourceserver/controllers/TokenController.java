d0581eca4229bdd5b0de78b58db323c4a505e45c
Francisco Sousa <franciscoeds1987@gmail.com>
2021-03-07 08:11:50 -0300

    Accessing JWT Claims

--- src/main/java/com/appsdeveloperblog/ws/api/resourceserver/controllers/TokenController.java
+++ src/main/java/com/appsdeveloperblog/ws/api/resourceserver/controllers/TokenController.java
@@ -6,16 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/token")
 public class TokenController {
 
     @GetMapping
-    public Map<String, Jwt> getToken(@AuthenticationPrincipal Jwt jwt) {
+    public Jwt getToken(@AuthenticationPrincipal Jwt jwt) {
 
-        return Collections.singletonMap("principal", jwt);
+        return jwt;
     }
 }
